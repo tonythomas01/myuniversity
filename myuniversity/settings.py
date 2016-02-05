976b6f08647bf2f7c2966b7500b64f6d79f281e5
tonythomas01 <01tonythomas@gmail.com>
2016-02-05 13:02:01 +0530

    Added the navbar, the dropdown works now

--- myuniversity/settings.py
+++ myuniversity/settings.py
@@ -100,4 +100,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (MEDIA_ROOT,)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
